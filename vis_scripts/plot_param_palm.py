0cc5862df419f09792eb02840b33465790d7e31e
Carolyn Begeman <cmbranecky@gmail.com>
2021-08-02 10:49:52 -0600

    viz scripts updates for large flux profile fig

--- vis_scripts/plot_param_palm.py
+++ vis_scripts/plot_param_palm.py
@@ -23,10 +23,13 @@ bboxanchor = (0.0,-0.25)
 s_hr = 3600
 logmin = 1.0e-20
 
-figsize2_wide = (5,2.5)
-figsize2_box = (5,3)
-figsize2 = (4.25,4)
-figsize3 = (8.5/3,3)
+col2_width = 12 #cm
+col1_width = 8.3 #cm
+figsize2_wide = (col2_width/2,2.5)
+figsize2_box = (col2_width/2,3)
+figsize1 = (col1_width,4)
+figsize2 = (col2_width/2,col2_width/2)
+figsize3 = (col2_width/3,col2_width/3)
 
 # define default colors
 col = ['#186086','#F5793A','#A95AA1','#85C0F9','#D14A42','#2ca25f','#99d8c9','#5F86DD']
